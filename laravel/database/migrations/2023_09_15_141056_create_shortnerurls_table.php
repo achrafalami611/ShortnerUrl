619ff57d55e662f91bdeb4a140bee904aa5d8a39
achrafalami611 <achrafalami611@gmail.com>
2023-09-15 20:59:32 +0100

    create table shortner url

--- laravel/database/migrations/2023_09_15_141056_create_shortnerurls_table.php
+++ laravel/database/migrations/2023_09_15_141056_create_shortnerurls_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('shortnerurls', function (Blueprint $table) {
             $table->id();
             $table->text('original_url');
+            $table->text('url_description')->nullable();
             $table->string('short_url', 10)->unique();
             $table->integer('visits_count')->default(0);
             $table->timestamps();
