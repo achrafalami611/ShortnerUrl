4e0201414e72402be42ef5437ca91088d5782331
achrafalami611 <achrafalami611@gmail.com>
2023-09-16 13:42:38 +0100

    Add Environment Variable =>  BackEnd && Call Pinia in main.ts

---
+++ vueJs/environment/environment.ts
@@ -0,0 +1,5 @@
+const {VITE_APP_API_BASE_URL } = import.meta.env;
+
+const apiBaseUrl = VITE_APP_API_BASE_URL;
+
+export { apiBaseUrl };
--- vueJs/src/main.ts
+++ vueJs/src/main.ts
@@ -3,11 +3,14 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import ElementPlus from 'element-plus'
 import router from '@/plugins/router'
+import { createPinia } from 'pinia';
 
 import './assets/style.css' 
 
 const app = createApp(App)
+const pinia = createPinia();
 
 app.use(router)
+app.use(pinia);
 app.use(ElementPlus)
 app.mount('#app')
