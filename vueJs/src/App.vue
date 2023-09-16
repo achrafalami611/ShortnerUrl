1cf6918a2e942523f1dd12375eeba6bd894dc4c1
achrafalami611 <achrafalami611@gmail.com>
2023-09-16 17:32:31 +0100

    Add style Globaly In App.vue

--- vueJs/src/App.vue
+++ vueJs/src/App.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts">
+<script setup>
+import SideBar from "@/common/components/SideBar.vue";
+
 </script>
 
 <template>
   <header>
+    <div class="bg-black text-white px-20 py-5">
+      <h1 class="text-xl font-bold">Shortner Url</h1>
+    </div>
   </header>
 
-  <main>
+  <div class="flex items-center justify-between">
+    <div>
+      <SideBar />
+    </div>
+
+    <main class="m-auto p-5">
     <RouterView />
   </main>
+  </div>
+
 </template>
+<style lang="scss" scoped>
+  main{
+    width: 95%;
+  }
+</style>
