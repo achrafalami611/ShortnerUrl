ac9f501d4e4ce0ec2aea70712bcc5480015d0dff
achrafalami611 <achrafalami611@gmail.com>
2023-09-16 01:59:18 +0100

    Add routes Api that i need to get and store data

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShortnerUrlController;
 
 
-
-
-Route::post('/shorten', 'UrlController@shorten');
-Route::get('/{shortUrl}', 'UrlController@redirect');
+Route::group(['prefix' => 'shortenUrl'], function () {
+    Route::get('/', [ShortnerUrlController::class, 'index'])->name('shortenUrl.index');
+    Route::post('/store', [ShortnerUrlController::class, 'store'])->name('shortenUrl.store');
+    Route::get('/show/{shortnerUrl}', [ShortnerUrlController::class, 'show'])->name('shortenUrl.show');
+});
 
 
