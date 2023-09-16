ab203b1075762cff1b9a04a944a68434a6641a3a
achrafalami611 <achrafalami611@gmail.com>
2023-09-16 19:26:24 +0100

    import some componenet / Add style form general

--- vueJs/src/App.vue
+++ vueJs/src/App.vue
@@ -5,17 +5,17 @@ import SideBar from "@/common/components/SideBar.vue";
 
 <template>
   <header>
-    <div class="bg-black text-white px-20 py-5">
+    <div class="bg-primary text-white px-20 py-5">
       <h1 class="text-xl font-bold">Shortner Url</h1>
     </div>
   </header>
 
-  <div class="flex items-center justify-between">
-    <div>
+  <div class="flex items-baseline justify-between">
+    <div class="pt-8">
       <SideBar />
     </div>
 
-    <main class="m-auto p-5">
+    <main class="m-auto p-8">
     <RouterView />
   </main>
   </div>
@@ -23,6 +23,6 @@ import SideBar from "@/common/components/SideBar.vue";
 </template>
 <style lang="scss" scoped>
   main{
-    width: 95%;
+    width: 99%;
   }
 </style>
