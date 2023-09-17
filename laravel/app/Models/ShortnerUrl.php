7bf3026fa96ca77f94bb637a81e0055152841731
achrafalami611 <achrafalami611@gmail.com>
2023-09-17 15:46:09 +0100

    format created_at global accessor in model

--- laravel/app/Models/ShortnerUrl.php
+++ laravel/app/Models/ShortnerUrl.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class ShortnerUrl extends Model
 {
@@ -11,5 +12,11 @@ class ShortnerUrl extends Model
 
     protected $guarded = ['id'];
     protected $table = 'shortnerurls';
+
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('d-m-Y H:i:s');
+    }
     
 }
